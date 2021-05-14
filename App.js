38fba9b41a5b27812de7452510bcbc278098f432
KKharayat <keertikharayat@gmail.com>
2021-05-14 23:52:41 +0530

    SearchList added

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import SearchList from './src/SearchList';
+
+class App extends Component {
+  render() {
+    return <SearchList />;
+  }
+}
+
+export default App;
